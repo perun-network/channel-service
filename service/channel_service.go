1f66897fb9a89ff531598b70a64efce231fab789
Jan Bormet <jan.bormet@bormet.info>
2023-10-23 12:44:04 +0200

    service: Implement User recognition from ChannelOpenRequest

--- service/channel_service.go
+++ service/channel_service.go
@@ -2,29 +2,54 @@ package service
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	address2 "github.com/nervosnetwork/ckb-sdk-go/v2/address"
+	"github.com/nervosnetwork/ckb-sdk-go/v2/rpc"
+	"github.com/nervosnetwork/ckb-sdk-go/v2/types"
 	"perun.network/channel-service/rpc/proto"
 	"perun.network/channel-service/wallet"
 	"perun.network/go-perun/channel"
 	gpwallet "perun.network/go-perun/wallet"
+	"perun.network/go-perun/watcher/local"
 	"perun.network/go-perun/wire"
 	"perun.network/go-perun/wire/net/simple"
 	"perun.network/go-perun/wire/protobuf"
+	"perun.network/perun-ckb-backend/backend"
+	"perun.network/perun-ckb-backend/channel/adjudicator"
+	"perun.network/perun-ckb-backend/channel/funder"
+	"perun.network/perun-ckb-backend/client"
+	"perun.network/perun-ckb-backend/wallet/address"
 	"perun.network/perun-ckb-backend/wallet/external"
 )
 
 type ChannelService struct {
 	UserRegister UserRegister
-	Wallet       gpwallet.Wallet
+	wsc          proto.WalletServiceClient
+	bus          wire.Bus
+	network      types.Network
+	node         rpc.Client
+	deployment   backend.Deployment
+	wallet       gpwallet.Wallet
 
 	proto.UnimplementedChannelServiceServer // always embed
 }
 
-func NewChannelService(c proto.WalletServiceClient) *ChannelService {
+func NewChannelService(c proto.WalletServiceClient, bus wire.Bus, network types.Network, nodeUrl string, deployment backend.Deployment) (*ChannelService, error) {
+	node, err := rpc.Dial(nodeUrl)
+	if err != nil {
+		return nil, err
+	}
+
 	return &ChannelService{
 		UserRegister: NewMutexUserRegister(),
-		Wallet:       external.NewWallet(wallet.NewExternalClient(c)),
-	}
+		wsc:          c,
+		bus:          bus,
+		network:      network,
+		node:         node,
+		deployment:   deployment,
+		wallet:       external.NewWallet(wallet.NewExternalClient(c)),
+	}, nil
 }
 
 func (c ChannelService) OpenChannel(ctx context.Context, request *proto.ChannelOpenRequest) (*proto.ChannelOpenResponse, error) {
@@ -64,8 +89,7 @@ func (c ChannelService) UpdateChannel(ctx context.Context, request *proto.Channe
 
 	return &proto.ChannelUpdateResponse{Msg: &proto.ChannelUpdateResponse_Update{Update: &proto.SuccessfulUpdate{
 		// TODO: Use actual resulting state instead of the request state.
-		State: request.State,
-		// TODO: Abstract channel id encoding.
+		State:     request.State,
 		ChannelId: cid[:],
 	}}}, nil
 }
@@ -106,21 +130,72 @@ func (c ChannelService) GetChannelInfoFromRequest(reqChannelId []byte) (channel.
 }
 
 func (c ChannelService) GetUserFromChannelOpenRequest(request *proto.ChannelOpenRequest) (*User, error) {
-	// FIXME: The OpenChannelRequest should contain info about the acting participant.
-	panic("fixme")
+	requester := request.GetRequester()
+	if requester == nil {
+		return nil, fmt.Errorf("missing requester in ChannelOpenRequest")
+	}
+	var addr address.Participant
+	err := addr.UnmarshalBinary(requester)
+	if err != nil {
+		return nil, err
+	}
+	usr, err := c.UserRegister.GetUserFromParticipant(addr)
+	if err == nil {
+		return usr, nil
+	}
+	if !errors.Is(err, ErrUserNotFound) { // TODO: Maybe we should create a new user in this case.
+		return nil, err
+	}
+	usr, err = c.InitializeUser(addr)
+	if err != nil {
+		return nil, err
+	}
+	return c.UserRegister.AddUser(addr, usr), nil
+}
+
+func (c ChannelService) InitializeUser(participant address.Participant) (*User, error) {
+	wAddr, err := c.MakeDefaultWireAddress(participant)
+	if err != nil {
+		return nil, err
+	}
+	rs := wallet.NewRemoteSigner(c.wsc, c.ToCKBAddress(participant))
+	ckbClient, err := client.NewClient(c.node, rs, c.deployment)
+	if err != nil {
+		return nil, err
+	}
+	f := funder.NewDefaultFunder(ckbClient, c.deployment)
+	adj := adjudicator.NewAdjudicator(ckbClient)
+	watcher, err := local.NewWatcher(adj)
+	if err != nil {
+		return nil, err
+	}
+	return NewUser(participant, wAddr, c.bus, f, adj, c.wallet, watcher, c.wsc)
 }
 
 func (c ChannelService) GetAllocationFromChannelOpenRequest(request *proto.ChannelOpenRequest) (*channel.Allocation, error) {
-	// FIXME: ChannelOpenRequest should specify an allocation / initial funds.
-	panic("fixme")
+	if request.GetAllocation() == nil {
+		return nil, fmt.Errorf("missing allocation in ChannelOpenRequest")
+	}
+	return protobuf.ToAllocation(request.GetAllocation())
 }
 
 func (c ChannelService) GetPeerAddressFromChannelOpenRequest(request *proto.ChannelOpenRequest) wire.Address {
-	// FIXME: Do this properly and change the peer type in the request field.
+	// NOTE: The peer address should probably be a string-encoded CKB Address (see MakeDefaultWireAddress).
 	return simple.NewAddress(string(request.GetPeer()))
 }
 
 func (c ChannelService) GetChallengeDurationFromChannelOpenRequest(request *proto.ChannelOpenRequest) uint64 {
-	// TODO: This should be specified in the request or hardcoded with default value.
-	return 10
+	return request.ChallengeDuration
+}
+
+func (c ChannelService) MakeDefaultWireAddress(participant address.Participant) (wire.Address, error) {
+	ckbAddr, err := c.ToCKBAddress(participant).Encode()
+	if err != nil {
+		return nil, err
+	}
+	return simple.NewAddress(ckbAddr), nil
+}
+
+func (c ChannelService) ToCKBAddress(addr address.Participant) address2.Address {
+	return addr.ToCKBAddress(c.network)
 }
